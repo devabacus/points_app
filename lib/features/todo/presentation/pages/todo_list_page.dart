1a594e25e7cee6fb6bf90b53a3e9e0940fb8ea9c
mrfrolk <ekaterita20@gmail.com>
2025-03-27 16:16:59 +0500

    сделал через stateful и isloading

--- lib/features/todo/presentation/pages/todo_list_page.dart
+++ lib/features/todo/presentation/pages/todo_list_page.dart
@@ -1,14 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:points_app/core/database/database.dart';
+import 'package:points_app/features/todo/data/models/task_model.dart';
+import 'package:points_app/features/todo/presentation/providers/shared_pref_provider.dart';
+import 'package:points_app/features/todo/presentation/providers/task_provider.dart';
 
-class TodoListPage extends ConsumerWidget {
+class TodoListPage extends ConsumerStatefulWidget {
   const TodoListPage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends ConsumerState<TodoListPage> {
+  List<TaskItem> allTasks = [];
+  bool isLoading = true;
+
+  Future<void> getAllTask() async {
+    try {
+      final taskStorage = ref.read(taskStorageProvider.notifier);
+      final tasks = await taskStorage.getAll();
+      setState(() {
+        allTasks = tasks;
+        isLoading = false;
+      });
+    } catch (_) {
+      isLoading = false;
+    }
+  }
+
+  @override
+  void initState() {
+    getAllTask();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Все задачи")),
-      body: Center(child: Text("")),
+      body:
+          isLoading
+              ? Center(child: CircularProgressIndicator())
+              : allTasks.isEmpty
+              ? Center(child: Text("Нет задач"))
+              : Center(
+                child: ListView.builder(
+                  itemCount: allTasks!.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(title: Text(allTasks![0].toString()));
+                  },
+                ),
+              ),
     );
   }
 }
